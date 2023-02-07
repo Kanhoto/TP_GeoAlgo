7d5667456ee3dac7c4733bbb356d17e9f5ffb077
vicanhotod <vicanhotod@a204pc20L.local.isima.fr>
2023-02-07 11:21:56 +0100

    [+]TP_GeoAlgo - adding modif in genre

--- src/genre.cpp
+++ src/genre.cpp
@@ -3,12 +3,74 @@
 #include <CGAL/IO/Polyhedron_iostream.h>
 #include <iostream>
 #include <fstream>
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Polyhedron_3.h>
+
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
 typedef Polyhedron::Facet_iterator Facet_iterator;
 typedef Polyhedron::Vertex_iterator Vertex_iterator;
 typedef Polyhedron::Halfedge_iterator Halfedge_iterator;
+//typedef CGAL::Simple_cartesian<double> Kernel;
+typedef Kernel::Point_3 Point_3;
+typedef Polyhedron::Halfedge_around_facet_circulator Halfedge_facet_circulator;
 
+unsigned int vertsCount(Polyhedron & poly_i){
+	unsigned int nbVerts = 0;
+	for (Vertex_iterator i = poly_i.vertices_begin(); i != poly_i.vertices_end(); ++i) {
+		++nbVerts;
+	}
+	std::cout << "Nombre de sommets: " << nbVerts << std::endl;
+
+	return nbVerts;
+}
+
+unsigned int edgesCount(Polyhedron & poly_i){
+	unsigned int nbEdges = 0;
+	for (Halfedge_iterator i = poly_i.halfedges_begin(); i != poly_i.halfedges_end(); ++i) {
+		++nbEdges;
+	}
+	nbEdges /= 2;
+	std::cout << "Nombre d'arêtes: " << nbEdges << std::endl;
+
+	return nbEdges;
+}
+
+unsigned int facesCount(Polyhedron & poly_i){
+	unsigned int nbFaces = 0;
+	for (Facet_iterator i = poly_i.facets_begin(); i != poly_i.facets_end(); ++i) {
+		++nbFaces;
+	}
+	std::cout << "Nombre de faces: " << nbFaces << std::endl;
+
+	return nbFaces;
+}
+
+void meshPerimeter(Polyhedron & poly_i){
+	// Write polyhedron in Object File Format (OFF).
+    CGAL::set_ascii_mode( std::cout);
+    std::cout << "OFF" << std::endl << poly_i.size_of_vertices() << ' '
+              << poly_i.size_of_facets() << " 0" << std::endl;
+    std::copy( poly_i.points_begin(), poly_i.points_end(),
+               std::ostream_iterator<Point_3>( std::cout, "\n"));
+    for (  Facet_iterator i = poly_i.facets_begin(); i != poly_i.facets_end(); ++i) {
+		float perimeter = 0;
+		Halfedge_facet_circulator prev = i->facet_begin();
+        Halfedge_facet_circulator curr = ++(i->facet_begin());
+        // Facets in polyhedral surfaces are at least triangles.
+        std::cout << CGAL::circulator_size(prev) << ' ';
+        do {
+            perimeter += CGAL::squared_distance(prev->vertex()->point(), curr->vertex()->point());
+			++prev;
+			++curr;
+        } while ( prev != i->facet_begin());
+        std::cout << perimeter << std::endl;
+    }
+}
+
+void exportCgalOffWithColor(std::ostream & out, std::map<Polyhedron::Facet_handle, double> & maillage){
+
+}
 
 int main(int argc, char* argv[])
 {
@@ -18,30 +80,17 @@ int main(int argc, char* argv[])
 	}
 	
 	Polyhedron mesh;
-  std::ifstream input(argv[1]);
-	 if (!input || !(input >> mesh) || mesh.is_empty()) {
-    std::cerr << "Le fichier donné n'est pas un fichier off valide." << std::endl;
-    return 1;
-  }
+  	std::ifstream input(argv[1]);
+	if (!input || !(input >> mesh) || mesh.is_empty()) {
+    	std::cerr << "Le fichier donné n'est pas un fichier off valide." << std::endl;
+    	return 1;
+  	}
   
-  unsigned int nbVerts = 0;
-	for (Vertex_iterator i = mesh.vertices_begin(); i != mesh.vertices_end(); ++i) {
-		++nbVerts;
-	}
-	std::cout << "Nombre de sommets: " << nbVerts << std::endl;
-	
-	unsigned int nbEdges = 0;
-	for (Halfedge_iterator i = mesh.halfedges_begin(); i != mesh.halfedges_end(); ++i) {
-		++nbEdges;
-	}
-	nbEdges /= 2;
-	std::cout << "Nombre d'arêtes: " << nbEdges << std::endl;
+  	unsigned int nbVerts = vertsCount(mesh);
+	unsigned int nbEdges = edgesCount(mesh);
+	unsigned int nbFaces = facesCount(mesh);
 
-	unsigned int nbFaces = 0;
-	for (Facet_iterator i = mesh.facets_begin(); i != mesh.facets_end(); ++i) {
-		++nbFaces;
-	}
-	std::cout << "Nombre de faces: " << nbFaces << std::endl;
+	meshPerimeter(mesh);
 	
 	unsigned int euler = nbVerts - nbEdges + nbFaces;
 	unsigned int genus = (2 - euler) / 2;
---
+++ src/measures.cpp
@@ -0,0 +1,195 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/boost/graph/graph_traits_Polyhedron_3.h>
+#include <CGAL/IO/Polyhedron_iostream.h>
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
+
+typedef Polyhedron::Facet_const_iterator Facet_iterator;
+typedef Polyhedron::Vertex_const_iterator Vertex_iterator;
+typedef Polyhedron::Halfedge_const_iterator Halfedge_iterator;
+typedef Polyhedron::Halfedge_around_facet_const_circulator Halfedge_facet_circulator;
+
+typedef std::map<Polyhedron::Facet_const_handle, double> Facet_double_map;
+typedef std::map<Polyhedron::Facet_const_handle, int> Facet_int_map;
+
+struct Color_t{
+	double red=0;
+	double green=0;
+	double blue=0;
+};
+
+std::vector<Color_t> colorClasses;
+
+void GenerateRandomColorClasses(std::vector<Color_t> & vec_i, int n){
+	Color_t c;
+	for(int i=0; i<n; ++i){
+		c.red = (double)rand() / RAND_MAX;
+		c.green = (double)rand() / RAND_MAX;
+		c.blue = (double)rand() / RAND_MAX;
+
+		vec_i.push_back(c);
+	}
+}
+
+/// @brief map all the values from [min, max] to [0, 1]
+/// @param facetMap non-const reference to the map (it is an in/out parameter)
+void normalizeMap(Facet_double_map &facetMap)
+{
+	double maxValue = facetMap.begin()->second;
+	double minValue = facetMap.begin()->second;
+
+	// look for min and max value in the map
+	for (const auto &elem : facetMap)
+	{
+		if (elem.second > maxValue)
+		{
+			maxValue = elem.second;
+		}
+		if (elem.second < minValue)
+		{
+			minValue = elem.second;
+		}
+	}
+
+	for (auto &elem : facetMap)
+	{
+		elem.second -= minValue;
+		elem.second /= (maxValue-minValue);
+	}
+}
+
+/// @brief Generate in a .off file a colored mesh according to a value map (green to red shades)
+/// @param mesh the input mesh
+/// @param facetMap map of values between 0 and 1 (see "normalize()") for each facet of mesh
+/// @param filePath path to the colored .off file to be generated
+void writeOFFfromValueMap(const Polyhedron& mesh, const Facet_double_map& facetMap, std::string filePath)
+{
+	std::ofstream in_myfile;
+	in_myfile.open(filePath);
+
+	CGAL::set_ascii_mode(in_myfile);
+
+	in_myfile << "COFF" << std::endl // "COFF" makes the file support color informations
+			  << mesh.size_of_vertices() << ' ' 
+			  << mesh.size_of_facets() << " 0" << std::endl; 
+			  // nb of vertices, faces and edges (the latter is optional, thus 0)
+
+	std::copy(mesh.points_begin(), mesh.points_end(),
+			  std::ostream_iterator<Kernel::Point_3>(in_myfile, "\n"));
+
+	int cpt=0;
+	for (Facet_iterator i = mesh.facets_begin(); i != mesh.facets_end(); ++i)
+	{
+		Halfedge_facet_circulator j = i->facet_begin();
+
+		CGAL_assertion(CGAL::circulator_size(j) >= 3);
+
+		in_myfile << CGAL::circulator_size(j) << ' ';
+		do
+		{
+			in_myfile << ' ' << std::distance(mesh.vertices_begin(), j->vertex());
+
+		} while (++j != i->facet_begin());
+
+		in_myfile << std::setprecision(5) << std::fixed; //set the format of floats to X.XXXXX
+
+		Color_t c;
+		c.red = 1- facetMap.at(i) * colorClasses[cpt].red; // low values will be closer to red
+		c.green = facetMap.at(i) * colorClasses[cpt].green; // high values will be closer to green
+		c.blue = facetMap.at(i) * colorClasses[cpt].blue;
+
+		in_myfile << " " << c.red << " " << c.green << " " << c.blue;
+
+		in_myfile << std::endl;
+		cpt = (++cpt)%15;
+	}
+
+	in_myfile.close();
+
+	std::cout << "Le résultat a été exporté dans " << filePath << " !" << std::endl;
+}
+
+Facet_double_map computePerimMap(const Polyhedron &mesh)
+{
+	Facet_double_map out;
+
+	for (Facet_iterator i = mesh.facets_begin(); i != mesh.facets_end(); ++i)
+	{
+		double current_perimeter = 0.;
+		Halfedge_facet_circulator j = i->facet_begin();
+		do
+		{
+			current_perimeter += std::sqrt(CGAL::squared_distance(j->vertex()->point(), j->opposite()->vertex()->point()));
+		} while (++j != i->facet_begin());
+
+		std::cout << "perim(" << std::distance(mesh.facets_begin(), i) << ")=" << current_perimeter << std::endl;
+
+		out[i] = current_perimeter;
+	}
+
+	return out;
+}
+
+Facet_double_map computeAreaMap(const Polyhedron &mesh)
+{
+	Facet_double_map out;
+
+	for (Facet_iterator i = mesh.facets_begin(); i != mesh.facets_end(); ++i)
+	{
+	
+		Halfedge_facet_circulator j = i->facet_begin();
+		
+		Polyhedron::Vertex_const_handle firstVertex = j->vertex();
+
+		double current_area = 0;
+		// a facet is not necessarily a triangle, so we decompose one facet into multiple triangles,
+		// and sum up all their areas. Only works for convex faces.
+		// (illustration: http://mathbitsnotebook.com/JuniorMath/Polygons/polygons3g.jpg)
+		do
+		{
+			current_area += CGAL::squared_area(firstVertex->point(), j->vertex()->point(), j->opposite()->vertex()->point()); 
+		} while (++j != i->facet_begin());
+
+		std::cout << "area(" << std::distance(mesh.facets_begin(), i) << ")=" << current_area << std::endl;
+
+		out[i] = current_area;
+	}
+
+	return out;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		std::cerr << "Il manque un paramètre au programme. Veuillez lui donner en entrée un nom de fichier au format off." << std::endl;
+		return 1;
+	}
+
+	Polyhedron mesh;
+
+	std::ifstream input(argv[1]);
+
+	if (!input || !(input >> mesh) || mesh.is_empty())
+	{
+		std::cerr << "Le fichier donné n'est pas un fichier off valide." << std::endl;
+		return 1;
+	}
+
+	auto mapPerim = computePerimMap(mesh);
+
+	const int numberOfClasses = 15;
+	GenerateRandomColorClasses(colorClasses, numberOfClasses);
+
+	normalizeMap(mapPerim);
+
+	writeOFFfromValueMap(mesh, mapPerim, argc>=3?argv[2]:"result.off");
+
+	return 0;
+}
+ 
