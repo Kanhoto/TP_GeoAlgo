ccc1dd6f2ddba3759c7ebe4515ae85aa906c9b14
vicanhotod <vicanhotod@a204pc20L.local.isima.fr>
2023-02-07 10:27:45 +0100

    [+]TP_GeoAlgo - configured

---
+++ build/src/CMakeFiles/measures.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/measures.dir/measures.cpp.o"
+  "measures"
+  "measures.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/measures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
