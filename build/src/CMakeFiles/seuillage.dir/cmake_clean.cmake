787b0876d18790fe348787492b71f6b7c0997627
vadinardo <vadinardo@a204pc21L.local.isima.fr>
2023-02-07 10:59:35 +0100

    com

---
+++ build/src/CMakeFiles/seuillage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../seuillage"
+  "../seuillage.pdb"
+  "CMakeFiles/seuillage.dir/seuillage.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/seuillage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
