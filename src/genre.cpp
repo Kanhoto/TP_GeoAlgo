db52645aeae161fed9bd846da7e910057f9fd5e9
vicanhotod <vicanhotod@a204pc20L.local.isima.fr>
2023-02-07 11:29:39 +0100

    [+]Tp_GeoAlgo(main) - first merge

--- src/genre.cpp
+++ src/genre.cpp
@@ -1,26 +1,16 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/boost/graph/graph_traits_Polyhedron_3.h>
 #include <CGAL/IO/Polyhedron_iostream.h>
-
 #include <iostream>
 #include <fstream>
-<<<<<<< HEAD
-
-typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
-typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
-=======
-<<<<<<< HEAD
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Polyhedron_3.h>
 
-=======
->>>>>>> val
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
 typedef Polyhedron::Facet_iterator Facet_iterator;
 typedef Polyhedron::Vertex_iterator Vertex_iterator;
 typedef Polyhedron::Halfedge_iterator Halfedge_iterator;
-<<<<<<< HEAD
 //typedef CGAL::Simple_cartesian<double> Kernel;
 typedef Kernel::Point_3 Point_3;
 typedef Polyhedron::Halfedge_around_facet_circulator Halfedge_facet_circulator;
@@ -38,39 +28,11 @@ unsigned int vertsCount(Polyhedron & poly_i){
 unsigned int edgesCount(Polyhedron & poly_i){
 	unsigned int nbEdges = 0;
 	for (Halfedge_iterator i = poly_i.halfedges_begin(); i != poly_i.halfedges_end(); ++i) {
-=======
->>>>>>> 87af73cb9093507404d7822fea600c015c0fb4a5
-
-typedef Polyhedron::Facet_const_iterator Facet_iterator;
-typedef Polyhedron::Vertex_const_iterator Vertex_iterator;
-typedef Polyhedron::Halfedge_const_iterator Halfedge_iterator;
-
-typedef std::map<Polyhedron::Facet_handle, double> Facet_double_map;
-typedef std::map<Polyhedron::Facet_handle, int> Facet_int_map;
-
-int computeGenus(const Polyhedron &mesh)
-{
-	unsigned int nbVerts = 0;
-	for (Vertex_iterator i = mesh.vertices_begin(); i != mesh.vertices_end(); ++i)
-	{
-		++nbVerts;
-	}
-	std::cout << "Nombre de sommets: " << nbVerts << std::endl;
-
-	unsigned int nbEdges = 0;
-<<<<<<< HEAD
-	for (Halfedge_iterator i = mesh.halfedges_begin(); i != mesh.halfedges_end(); ++i)
-	{
-=======
-	for (Halfedge_iterator i = mesh.halfedges_begin(); i != mesh.halfedges_end(); ++i) {
->>>>>>> val
->>>>>>> 87af73cb9093507404d7822fea600c015c0fb4a5
 		++nbEdges;
 	}
 	nbEdges /= 2;
 	std::cout << "Nombre d'arêtes: " << nbEdges << std::endl;
 
-<<<<<<< HEAD
 	return nbEdges;
 }
 
@@ -129,45 +91,11 @@ int main(int argc, char* argv[])
 	unsigned int nbFaces = facesCount(mesh);
 
 	meshPerimeter(mesh);
-=======
-	unsigned int nbFaces = 0;
-	for (Facet_iterator i = mesh.facets_begin(); i != mesh.facets_end(); ++i)
-	{
-		++nbFaces;
-	}
-	std::cout << "Nombre de faces: " << nbFaces << std::endl;
-<<<<<<< HEAD
-
-=======
->>>>>>> val
 	
->>>>>>> 87af73cb9093507404d7822fea600c015c0fb4a5
 	unsigned int euler = nbVerts - nbEdges + nbFaces;
 	unsigned int genus = (2 - euler) / 2;
 	std::cout << "En supposant que le maillage contienne une unique surface sans bord, alors son genre est de " << genus << std::endl;
-
-	return genus;
-}
-
-int main(int argc, char *argv[])
-{
-	if (argc < 2)
-	{
-		std::cerr << "Il manque un paramètre au programme. Veuillez lui donner en entrée un nom de fichier au format off." << std::endl;
-		return 1;
-	}
-
-	Polyhedron mesh;
-
-	std::ifstream input(argv[1]);
-	
-	if (!input || !(input >> mesh) || mesh.is_empty())
-	{
-		std::cerr << "Le fichier donné n'est pas un fichier off valide." << std::endl;
-		return 1;
-	}
-
-	computeGenus(mesh);
-
+  
+  
 	return 0;
 }
