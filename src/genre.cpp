151d49560427554d92c2e12f8494ced9b1ffdcf3
vadinardo <vadinardo@a204pc21L.local.isima.fr>
2023-02-28 10:32:54 +0100

    jsp

--- src/genre.cpp
+++ src/genre.cpp
@@ -1,39 +1,6 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/boost/graph/graph_traits_Polyhedron_3.h>
 #include <CGAL/IO/Polyhedron_iostream.h>
-<<<<<<< HEAD
-#include <iostream>
-#include <fstream>
-typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
-typedef CGAL::Polyhedron_3<Kernel> Polyhedron;
-typedef Polyhedron::Facet_iterator Facet_iterator;
-typedef Polyhedron::Vertex_iterator Vertex_iterator;
-typedef Polyhedron::Halfedge_iterator Halfedge_iterator;
-
-
-int main(int argc, char* argv[])
-{
-	if (argc < 2) {
-		std::cerr << "Il manque un paramètre au programme. Veuillez lui donner en entrée un nom de fichier au format off." << std::endl;
-		return 1;
-	}
-	
-	Polyhedron mesh;
-  std::ifstream input(argv[1]);
-	 if (!input || !(input >> mesh) || mesh.is_empty()) {
-    std::cerr << "Le fichier donné n'est pas un fichier off valide." << std::endl;
-    return 1;
-  }
-  
-  unsigned int nbVerts = 0;
-	for (Vertex_iterator i = mesh.vertices_begin(); i != mesh.vertices_end(); ++i) {
-		++nbVerts;
-	}
-	std::cout << "Nombre de sommets: " << nbVerts << std::endl;
-	
-	unsigned int nbEdges = 0;
-	for (Halfedge_iterator i = mesh.halfedges_begin(); i != mesh.halfedges_end(); ++i) {
-=======
 
 #include <iostream>
 #include <fstream>
@@ -60,25 +27,12 @@ int computeGenus(const Polyhedron &mesh)
 	unsigned int nbEdges = 0;
 	for (Halfedge_iterator i = mesh.halfedges_begin(); i != mesh.halfedges_end(); ++i)
 	{
->>>>>>> origin/val
 		++nbEdges;
 	}
 	nbEdges /= 2;
 	std::cout << "Nombre d'arêtes: " << nbEdges << std::endl;
 
 	unsigned int nbFaces = 0;
-<<<<<<< HEAD
-	for (Facet_iterator i = mesh.facets_begin(); i != mesh.facets_end(); ++i) {
-		++nbFaces;
-	}
-	std::cout << "Nombre de faces: " << nbFaces << std::endl;
-	
-	unsigned int euler = nbVerts - nbEdges + nbFaces;
-	unsigned int genus = (2 - euler) / 2;
-	std::cout << "En supposant que le maillage contienne une unique surface sans bord, alors son genre est de " << genus << std::endl;
-  
-  
-=======
 	for (Facet_iterator i = mesh.facets_begin(); i != mesh.facets_end(); ++i)
 	{
 		++nbFaces;
@@ -112,6 +66,5 @@ int main(int argc, char *argv[])
 
 	computeGenus(mesh);
 
->>>>>>> origin/val
 	return 0;
 }
